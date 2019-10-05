36875e62cd37e66945d7b98fbf5dd8007a71e4e3
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2019-10-05 13:37:07 -0700

    Refactoring.

--- src/main/java/slowraytracer/Main.java
+++ src/main/java/slowraytracer/Main.java
@@ -3,6 +3,7 @@ package slowraytracer;
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+import java.util.OptionalInt;
 import java.util.Random;
 import java.util.stream.Stream;
 import javax.imageio.ImageIO;
@@ -44,10 +45,15 @@ public final class Main {
                 final var directionX = (x + 0.5f) - halfWidth;
                 final var directionY = (y + 0.5f) - halfHeight;
                 final var viewDirection = new Vector3(directionX, directionY, directionZ).normalize();
-                if (sphere.intersects(new Ray(Vector3.ZERO, viewDirection))) {
-                    pixmap.set(x, y, sphere.material().diffuseColor());
+                final var colorOptional = castRay(new Ray(Vector3.ZERO, viewDirection), sphere);
+                if (colorOptional.isPresent()) {
+                    pixmap.set(x, y, colorOptional.getAsInt());
                 }
             }
         }
+    }
+
+    private static OptionalInt castRay(final Ray ray, final Sphere sphere) {
+        return sphere.intersects(ray) ? OptionalInt.of(sphere.material().diffuseColor()) : OptionalInt.empty();
     }
 }
